779b2b2c1931ffee504401dcb80fc2062eaba463
avariar27 <lavenderblossom3@gmail.com>
2022-09-02 20:22:16 -0400

    Rosalind problems 2 and 3: Transcribing DNA and complementing the reverse of the DNA string.

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
-
-
 import argparse
 import os
+import pathlib
+
 from typing import NamedTuple
 
 class Args(NamedTuple):
@@ -18,6 +18,7 @@ def get_args() -> Args:
         args.dna = open(args.dna).read().rstrip()
     return Args(args.dna)
 
+# Counting DNA Nucleotides
 def countIt() -> None:
     args = get_args()
     a_count, c_count, g_count, t_count = 0, 0, 0, 0
@@ -36,6 +37,39 @@ def countIt() -> None:
 
     print(a_count, c_count, g_count, t_count)
 
+# DNA to RNA converter
+def dna_to_rna(dna_seq):
+    return dna_seq.replace('T', 'U')
+
+def read_input(filename):
+    current_dir = pathlib.Path(__file__).parent
+    with open(f'{current_dir}/' + filename) as fp:
+        input_data = fp.read()
+    return input_data
+
+def get_compl_strand(dna_strand_1):
+    dna_compliments = {'G': 'C', 'C': 'G', 'T': 'A', 'A': 'T'}
+    dna_strand_2 = ''
+    n = len(dna_strand_1)
+    for idx in range(n - 1, -1, -1):
+        nuclio = dna_strand_1[idx]
+        dna_strand_2 += dna_compliments[nuclio]
+    return dna_strand_2
+
 if __name__ == '__main__':
+    # Counting DNA Nucleotides
+    print('******* Counting the nucleotides in a DNA String *******')
     countIt()
+
+    print('--------------------------------------------')
+
+    # DNA to RNA
+    print('********* Converting a DNA String to a RNA String ***********')
+    in_data = read_input('prob_2_input.txt')
+    print(dna_to_rna(in_data))
+
+    # Compliment a DNA strand
+    print('********* Determining the Reverse Compliment of a DNA String ***********')
+    in_data = read_input('prob_3_input.txt')
+    print(get_compl_strand(in_data))
 
